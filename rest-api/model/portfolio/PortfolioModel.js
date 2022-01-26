f499fe2f168a1c621733daa2af4491372ab47c54
Erick Rick <erickcredjes@gmail.com>
2022-01-25 21:33:05 -0300

    Correction of update query, creation of delete and trash querys in model

--- rest-api/model/portfolio/PortfolioModel.js
+++ rest-api/model/portfolio/PortfolioModel.js
@@ -3,7 +3,11 @@ const Connection = require("../../database/Connection");
 class PortfolioModel {
 
    static findAll(call) {
-      return Connection.query("SELECT * FROM portfolio LIMIT 100", call);
+      return Connection.query("SELECT * FROM portfolio WHERE lixeira=0 LIMIT 100", call);
+   }
+
+   static findTrash(call) {
+      return Connection.query("SELECT * FROM portfolio WHERE lixeira=1 LIMIT 100", call);
    }
 
    static findById(id, call) {
@@ -15,7 +19,15 @@ class PortfolioModel {
    }
 
    static update(portfolio, id, call) {
-      return Connection.query(`UPDATE portfolio descricao=${portfolio.descricao} detalhes=${portfolio.detalhes} WHERE id=${id}`, call)
+      return Connection.query(`UPDATE portfolio SET descricao=? , detalhes=? WHERE id_portfolio=${id}`, [portfolio.descricao, portfolio.detalhes], call)
+   }
+
+   static trash(id, call) {
+      return Connection.query(`UPDATE portfolio SET lixeira=1 WHERE id_portfolio=${id}`, call);
+   }
+
+   static delete(id, call) {
+      return Connection.query(`DELETE FROM portfolio WHERE id_portfolio=${id}`, call);
    }
 }
 
